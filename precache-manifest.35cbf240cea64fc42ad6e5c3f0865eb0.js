a64ede0b5192c2f1fc27ca95136d0cabe8fbe3c7
John <52072102+Roxy44@users.noreply.github.com>
2023-03-27 18:53:23 +0700

    Updates

---
+++ precache-manifest.35cbf240cea64fc42ad6e5c3f0865eb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36888bb5633f8c727191",
+    "url": "/VendingMachine/static/css/main.1074c32b.chunk.css"
+  },
+  {
+    "revision": "36888bb5633f8c727191",
+    "url": "/VendingMachine/static/js/main.aa366368.chunk.js"
+  },
+  {
+    "revision": "0bbb0d0e2062377eb41b",
+    "url": "/VendingMachine/static/js/runtime~main.becc816d.js"
+  },
+  {
+    "revision": "83226728460d8b9a36dc",
+    "url": "/VendingMachine/static/js/2.ff76d6a5.chunk.js"
+  },
+  {
+    "revision": "7035e116c55151aa59b4648965d45a29",
+    "url": "/VendingMachine/index.html"
+  }
+];
